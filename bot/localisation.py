8eaa2b5d2e249fdddb2e5ade98feb408b886723c
im-vivektomar <86093210+im-vivektomar@users.noreply.github.com>
2021-07-02 12:19:48 +0530

    Update localisation.py

--- bot/localisation.py
+++ bot/localisation.py
@@ -46,7 +46,7 @@ class Localisation:
     
     HELP_MESSAGE = get_config(
         "STRINGS_HELP_MESSAGE",
-        "How to Use Me? Follow These steps! \n\n1. Send me your Telegram Video. \n2. Reply the file - /compress And Persentage \nEg: <code>/compress 50</code> \n\nIf bot didn't respond, contact @synuser"
+        "How to Use Me? Follow These steps! \n\n1. Send me your Telegram Video. \n2. Reply the file - /compress And Percentage \nEg: <code>/compress 50</code> \n\nIf bot didn't respond @im_vivek_tomar"
     )
     WRONG_MESSAGE = get_config(
         "STRINGS_WRONG_MESSAGE",
